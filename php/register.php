ef89c3177013d579e17564bbe2bcdeeabca40713
Nicholas Kajoh <kajohterna@gmail.com>
2017-12-09 06:47:08 +0100

    fix: supply variable by reference

--- php/register.php
+++ php/register.php
@@ -3,9 +3,11 @@
 
   db_connect();
 
+  $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
   $sql = "INSERT INTO users (username, password, location) VALUES (?, ?, ?)";
   $statement = $conn->prepare($sql);
-  $statement->bind_param('sss', $_POST['username'], password_hash($_POST['password'], PASSWORD_DEFAULT), $_POST['location']);
+  $statement->bind_param('sss', $_POST['username'], $hashed_password, $_POST['location']);
 
   if ($statement->execute()) {
     redirect_to("/index.php?registered=true");
